8f99d58c5bdbfac2a73b180aca5f79dd5775b558
Tomás Salina <salinatomass53@gmail.com>
2021-12-02 18:43:12 -0300

    Google analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-// import { Router } from '@reach/router';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/home';
 import './styles/style.css';
+import Home from './pages/home';
 import TopPicks from './pages/top-picks';
 
+import ReactGA from 'react-ga';
+
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize('G-27ESG3M8V8');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
